c209cf02996a0ec8d9581b0dfc0d05644929fc32
Camila Pérez Mosquera <camila.perez@iberant.es>
2022-04-13 20:28:19 -0400

    show select correct

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  validates_presence_of [:name,  :image , :description, :stock, :price]
   belongs_to :brand
   has_one_attached :image
   has_rich_text :description
